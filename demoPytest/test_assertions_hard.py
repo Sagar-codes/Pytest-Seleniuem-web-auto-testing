17b4b65cbcc79014cb04f9dc8ba1d4c462c5d7fb
Sagar-codes <paulsagar5433@gmail.com>
2023-08-27 21:47:10 +0530

    First commit

---
+++ demoPytest/test_assertions_hard.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class AssertionTest():
+    pass
+
+def test_lambdatest_radio_button_demo_value():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get("https://www.lambdatest.com/selenium-playground/radiobutton-demo")
+    driver.find_element(By.XPATH, 
+    "//h4[contains(text(), 'Gender : ')]"
+    "//following::input[@value='Male']").click()
+
+    driver.find_element(By.XPATH, 
+    "//h4[contains(text(), 'Age : ')]"
+    "//following::input[@value='15 - 50']").click()
+
+    driver.find_element(By.XPATH, 
+    "//button[text()='Get values']").click()
+
+    gender  = driver.find_element(By.CSS_SELECTOR,
+            ".genderbutton").text
+    
+    age_group  = driver.find_element(By.CSS_SELECTOR,
+            ".groupradiobutton").text
+    
+    print("Gender : ", gender)
+    print("Age Group : ", age_group)
+                        
+    assert gender == 'Male', "Gender is Not Correct"
+
+    assert driver.title.__contains__("Selenium Grid Online")
+
+    assert "51" in age_group, "Age Group Is Not Correct"
